4b132dafa31b541990334ecd690182b04307e3dd
Adam Walker <adamw207@yahoo.com>
2021-01-23 18:18:14 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,10 +15,4 @@ class EmailAddressParser
 @parse.split(/,?\s/).uniq 
 end
 end 
-#    email_array = @email.split(/[, ]/).uniq
-#    email_array.reject! {|element| element.empty?}
-#    email_array
-#  end
-
-#end
 
